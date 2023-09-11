021e0e49102f5055b322517cf21deeaacdffc4c5
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-09-11 15:43:22 -0300

    Criando o elemento de inserção de vídeos com o JS

---
+++ js/criarVideo.js
@@ -0,0 +1,16 @@
+import { conectaApi } from "./conectaApi.js";
+const formulario = document.querySelector("[data-formulario]");
+
+async function criarVideo(evento) {
+    evento.preventDefault();
+
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 100).toString();
+    const url = document.querySelector("[data-url]").value;
+    const imagem = document.querySelector("[data-imagem]").value;
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento));
